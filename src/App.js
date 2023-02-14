1148e8a68e1ad04d0dadf9429c19087fc786e637
Matheus <matheuspl92@hotmail.com>
2023-02-14 16:54:18 -0300

    Import Bootstrap React Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         Hello World!
+        <Button variant='primary'>Click here</Button>
       </div>
     )
   }
