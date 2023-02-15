314e6cc1bc87bb1ed8729f10d682c95bd9f4c0d9
Matheus <matheuspl92@hotmail.com>
2023-02-15 12:54:25 -0300

    Change project structure and add CSS stylesheets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import { Component } from "react";
-import { Container, Row } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Content from "./components/content/Content";
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Header />
-        </Row>
-        <Row>
-          <Content />
-        </Row>
-        <Row>
-          <Footer />
-        </Row>
-      </Container>
+      <div id="main-container">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
     )
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Component } from "react";
 import { Container } from "react-bootstrap";
+import './Header.css';
 
 export default class Header extends Component {
     render() {
         return (
-            <Container>
-                <h1>CV Maker App</h1>
+            <Container id="header">
+                <h1 id="header-title">CV Maker App</h1>
             </Container>
         )
     }
