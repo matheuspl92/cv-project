26b5cfeddb383f67d0c05df680a9168afad543ae
Matheus <matheuspl92@hotmail.com>
2023-02-28 21:45:36 -0300

    Refactor App to functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Content from "./components/content/Content";
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div id="main-container">
-        <Header />
-        <Content />
-        <Footer />
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <div id="main-container">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
