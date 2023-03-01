a9fa662fc501571a24cf467c4b19bab9a110b41d
Matheus <matheuspl92@hotmail.com>
2023-02-28 21:01:26 -0300

    Refactor Header to functional component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Component } from "react";
 import { Container } from "react-bootstrap";
 import './Header.css';
 
-export default class Header extends Component {
+const Header = () => {
+
+    return (
+        <Container id="header">
+            <h1 id="header-title">CV Maker App</h1>
+        </Container>
+    );
+};
+
+export default Header;
+
+/* export default class Header extends Component {
     render() {
         return (
             <Container id="header">
@@ -11,4 +21,4 @@ export default class Header extends Component {
             </Container>
         )
     }
-}
+} */
